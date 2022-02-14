abc9aff056c8bcd90b12f26651bd21d8114de62c
andre <andredeveloper34@gmail.com>
2022-02-14 14:00:15 -0300

    atualização projeto urna

--- urna/etapas.js
+++ urna/etapas.js
@@ -1,51 +1,68 @@
 let etapas = [
     {
-        titulo: "DEPUTADO FEDERAL",
-        numeros: 5,
-        candidatos: [
-            {
-                numero: '38111',
-                nome: "Jair Messias Bolsonaro",
-                partido: "Partido Liberal PL",
-                fotos:[
-                    {url:'Bolsonaro.jpg', legenda: 'Deputado Federal'}
-                ]
-               
-            },
-            {
-                numero: '77222',
-                nome: 'Renam Calheiros ',
-                partido: 'MDB',
-                fotos:[
-                    {url:'Renam.jpg', legenda: 'Deputado Federal'}
-                ]
-            }, //Termino de Etapas de Vereador
-            {
-                titulo:'PREFEITO',
-                numeros: 2,
-                candidatos: [
-                    {
-                        numero: '13',
-                        nome: 'Luiz Inacio Lula da Silva',
-                        partido: 'Partido dos Trabalhadores',
-                        vice: 'Geraldo Alckmin ',
-                        fotos:[
-                            {url:'lula_ladrao.jpg', legenda: 'Prefeito'},
-                            {url:'Alckmin.jpg', legenda: 'Vice-Prefeito', small: true}
-                        ]
-                    },
-                    {
-                        numero: '17',
-                        nome: 'Flavio Bolsonaro ',
-                        partido: 'PSOL',
-                        vice: 'Fabricio Queiroz',
-                        fotos:[
-                            {url:'84.png',legenda:'Prefeito'},
-                            {url:'84_2.jpg',legenda: 'Vice-Prefeito', small: true}
-                        ]
-                    },
-                ]
-            }
-        ]
-    }
+    titulo: "DEPUTADO FEDERAL",
+    numeros: 5,
+    candidatos: [{
+            numero: '38111',
+            nome: "Jair Messias Bolsonaro",
+            partido: "Partido Liberal PL",
+            fotos: [{
+                url: 'Bolsonaro.jpg',
+                legenda: 'Deputado Federal'
+            }]
+
+        },
+        {
+            numero: '77222',
+            nome: 'Renam Calheiros ',
+            partido: 'MDB',
+            fotos: [{
+                url: 'Renam.jpg',
+                legenda: 'Deputado Federal'
+            }]
+
+        } //Termino de Etapas de Vereador
+    ]
+},
+
+
+
+{
+    titulo:'PREFEITO',
+    numeros: 2,
+    candidatos: [{
+            numero: '13',
+            nome: 'Luiz Inacio Lula da Silva',
+            partido: 'Partido dos Trabalhadores',
+            vice: 'Geraldo Alckmin ',
+            fotos: [{
+                    url: 'lula_ladrao.jpg',
+                    legenda: 'Prefeito'
+                },
+                {
+                    url: 'Alckmin.jpg',
+                    legenda: 'Vice-Prefeito',
+                    small: true
+                }
+            ]
+        },
+        {
+            numero: '17',
+            nome: 'Flavio Bolsonaro ',
+            partido: 'PSOL',
+            vice: 'Fabricio Queiroz',
+            fotos: [{
+                    url: '84.png',
+                    legenda: 'Prefeito'
+                },
+                {
+                    url: '84_2.jpg',
+                    legenda: 'Vice-Prefeito',
+                    small: true
+                }
+            ]
+        },
+    ]
+}
+
 ];
--- urna/scrpt.js
+++ urna/scrpt.js
@@ -9,11 +9,18 @@
  * se eu remover o '.d-2' completamente com o comando: aviso.style.display = 'none' removerei a informação de aviso;
  * */
 
-
+// VARIAVEIS DE AMBIENTE:
+let etapaAtual = 0;
+let numero = '';
+let votoBranco = false;
+let control = document.getElementsByClassName('d-1-3')[0];
+let fim = document.getElementsByClassName('d-1')[0];
+let descricaoFim = document.getElementsByClassName('d-2')[0];
 let seuVotoPara = document.querySelector('.d-1-1 span') //aqui estou selecionando dentro da classe d-1-1 a tag span (SEU VOTO PARA)
 let cargo = document.querySelector('.d-1-2 span') //aqui estou selecionando o cargo de Vereador
 let descricao = document.querySelector('.d-1-4')
 let aviso = document.querySelector('.d-2')
+
 
 /**
  * Controlando a parte Lateral de .d-1-right
@@ -22,7 +29,6 @@ let aviso = document.querySelector('.d-2')
  * controlando a area dos numeros:
  * numeros.innerHTML = ""; eu removo aos Numeros;
  * *  */
-
 let lateral = document.querySelector('.d-1-right')
 let numeros = document.querySelector('.d-1-3');
 
@@ -34,7 +40,7 @@ let numeros = document.querySelector('.d-1-3');
 function Clicou(n) {
     //irei verificar se tem um quadradinho com o pisca ativado:
     let elNumero = document.querySelector('.numero.pisca');
-    if(elNumero != null) {  //se ele for diferente de nullo, ele esta apto para adicionar
+    if (elNumero != null) { //se ele for diferente de nullo, ele esta apto para adicionar
         elNumero.innerHTML = n;
 
         //agora estou concatenando o proprio numero com o n
@@ -48,54 +54,73 @@ function Clicou(n) {
         elNumero.classList.remove('pisca');
         //fazendo uma verificação dos itens selcionados
 
-        if(elNumero.nextElementSibling !== null){  //se for diferente de null, tem o pisca.
-        elNumero.nextElementSibling.classList.add('pisca');
-        }else{
+        if (elNumero.nextElementSibling !== null) { //se for diferente de null, tem o pisca.
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
             //vou chamar a função atualiza inteface:
             atualizaInterface();
         }
 
     }
-   
+
 }
 
-function Branco(){
+function Branco() {
     //processo de Voto em Branco
-   
-        numero = '';
-        votoBranco = true;
-        seuVotoPara.style.display = 'block';
-        aviso.style.display = 'block';
-        numero.innerHTML = '';
-        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
-        lateral.innerHTML = '';
-        control.style.display = 'none';
-        //console.log(document.getElementsByClassName('d-1-3')[0]);
-       
+
+    numero = '';
+    votoBranco = true;
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    numero.innerHTML = '';
+    descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+    lateral.innerHTML = '';
+    control.style.display = 'none';
+    //console.log(document.getElementsByClassName('d-1-3')[0]);
+
 }
 
-function Corrige(){
+function Corrige() {
     //estou chamado a function começar etapa para zerar a minha urna:
     comecarEtapa();
-    
-}
 
-function Confirma(){
-    alert("clicou em CONFIRMA !");
 }
+//confimando o voto, posso enviar minhas informações para um webservice.
 
+function Confirma() {
+
+    let etapa = etapas[etapaAtual];
+
+    let votoConf = false;
+
+    if (votoBranco = false) {
+        votoBranco = true;
+        console.log("Confirmando Voto em BRANCO...!");
+
+    } else if (numero.length === etapa.numeros) {
+        votoConf = true;
+        console.log("Confirmando como " +numero);
+    }
+
+
+
+    if (votoConf) {
+        etapaAtual++;  //0  
+        if(etapas[etapaAtual] !== undefined) {
+            comecarEtapa();
+        } else {
+
+            fim.innerHTML = '<div class="aviso--gigante pisca"> FIM </div>';
+            descricaoFim.innerHTML = '<div class="msg--desc">Você Votou Num Ladrão</div>';
+        }
+    }
+}
+comecarEtapa();
 /**
  * Começando com as variaveis de Ambiente, sempre iremos começar da etapa 0:
  * NOTA: de posse dessa variavel, eu consigo saber tudo sobre o estado atual.
  * então se fizermos: etapas[etapaAtual] no item do meu array! eu vou ter = {titulo: "VEREADOR", numeros: 5, candidatos: array(2)}
  */
-
-// AMBIENTE:
-let etapaAtual = 0;
-let numero = '';
-let votoBranco = false;
-let control = document.getElementsByClassName('d-1-3')[0];
-
 
 /**
  * Vamos criar uma função chamada começar etapa:
@@ -105,88 +130,82 @@ let control = document.getElementsByClassName('d-1-3')[0];
  */
 
 //FUNÇÃO:
-function comecarEtapa(){
+function comecarEtapa() {
 
-let etapa = etapas[etapaAtual];
-control.style.display = 'block';
+    let etapa = etapas[etapaAtual];
+    control.style.display = 'block';
 
-// Dentro da variavel etapa eu tenho meu array da chave;
-let numeroHTML = '';
-numero = '';
-votoBranco = false;
-//agora vamos criar um looping que vai rodar 5 vezes, e que cada uma dessas vezes, vamos montar o html:
+    // Dentro da variavel etapa eu tenho meu array da chave;
+    let numeroHTML = '';
+    numero = '';
+    votoBranco = false;
+    //agora vamos criar um looping que vai rodar 5 vezes, e que cada uma dessas vezes, vamos montar o html:
 
-for(let i=0; i<etapa.numeros;i++) {
-    if(i === 0) {
-        numeroHTML = '<div class="numero pisca"></div>'
-    }else{
-        numeroHTML += '<div class="numero"></div>';
-        //aqui no final irei ter 05 caras de numeros
+    for (let i = 0; i < etapa.numeros; i++) {
+        if (i === 0) {
+            numeroHTML = '<div class="numero pisca"></div>'
+        } else {
+            numeroHTML += '<div class="numero"></div>';
+            //aqui no final irei ter 05 caras de numeros
+        }
+
     }
-      
-}
 
-seuVotoPara.style.display = 'none';
-cargo.innerHTML = etapa.titulo;
-// note que aqui estou buscando a chave titulo do arquivo etapas.js
-descricao.innerHTML = '';  
-aviso.style.display = 'none';  
-lateral.innerHTML = '';   
-numeros.innerHTML = numeroHTML;  //Estou preenchendo ela acima linha 49;
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    // note que aqui estou buscando a chave titulo do arquivo etapas.js
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHTML; //Estou preenchendo ela acima linha 49;
 }
-
 
 /**
  * Criando uma função de Atualização de interface, onde ele pega a variavel let numeros;
  * Vou executar ela toda vez que fizer uma ação: digitar um numero ele vai preencher o numero e atualizar as informações
  *  */
+function atualizaInterface() {
+    //console.log(numero);
+    //agora vou procurar um candidato que tenha na variavel numero:
 
+    let etapa = etapas[etapaAtual];
 
+    //fazemdo a Busca pelo meu candidato:
 
-function atualizaInterface(){
-     //console.log(numero);
-     //agora vou procurar um candidato que tenha na variavel numero:
-
-     let etapa = etapas[etapaAtual];
-
-     //fazemdo a Busca pelo meu candidato:
-
-     let candidato = etapa.candidatos.filter((item)=>{
+    let candidato = etapa.candidatos.filter((item) => {
         //vamos verificar se o item. candidatos do meu array etapas.js e igual ao numero que digitei:
 
-        if(item.numero === numero)
-        {
+        if (item.numero === numero) {
             return true;
-        }else{
+        } else {
             return false;
         }
-     });
-     if(candidato.length > 0 ) {
-         //ou seja se ele achou algum candidato:
-         candidato = candidato[0];
-         //seu voto vai aparecer agora:
-         seuVotoPara.style.display = 'block';
-         //aviso aparece:
-         aviso.style.display = 'block';
-         //descrição do cara
-         descricao.innerHTML = `Nome:${ candidato.nome}<br/>Partido:${candidato.partido}`;
-         //foto do camarada:
-         let fotosHtml = '';
-         for(let i in candidato.fotos){
-             fotosHtml += `<div class="d-1-image"><img src="../urna/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
-         }
-         lateral.innerHTML = fotosHtml;
+    });
+    if (candidato.length > 0) {
+        //ou seja se ele achou algum candidato:
+        candidato = candidato[0];
+        //seu voto vai aparecer agora:
+        seuVotoPara.style.display = 'block';
+        //aviso aparece:
+        aviso.style.display = 'block';
+        //descrição do cara
+        descricao.innerHTML = `Nome:${candidato.nome}<br/>Partido:${candidato.partido}`;
+        //foto do camarada:
+        let fotosHtml = '';
+        for (let i in candidato.fotos) {
+            fotosHtml += `<div class="d-1-image"><img src="../urna/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+        }
+        lateral.innerHTML = fotosHtml;
 
-     } else {
-         //Se não achar um candidato então o voto sera Nulo:
-         seuVotoPara.style.display = 'block';
-         aviso.style.display = 'block';
-         descricao.innerHTML = '<div class="aviso--grande pisca"> VOTO NULO </div>'
+    } else {
+        //Se não achar um candidato então o voto sera Nulo:
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso-- pisca"> VOTO NULO </div>'
 
-     }
+    }
 
-     console.log("Candidato", candidato);
-     
+    console.log("Candidato", candidato);
+
 }
-
 comecarEtapa();
